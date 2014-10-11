eb52851bb37cd3e23d768ad2e3a31d2db4745363
Caleb Davenport <calebmdavenport@gmail.com>
2014-10-11 13:53:55 -0400

    Start work on provider documentation.

--- application.rb
+++ application.rb
@@ -1,6 +1,15 @@
-require 'sinatra'
-require 'sinatra/content_for'
+set :markdown, fenced_code_blocks: true, highlight: true
 
 get '/' do
   erb :home
+end
+
+get '/providers/:name' do
+  name = params['name']
+  not_found and return unless File.exists? File.join(settings.root, "views/providers/#{name}.markdown")
+  erb :layout, layout: false do
+    erb :provider do
+      markdown "providers/#{name}".to_sym
+    end
+  end
 end
